ae37b2ef92d5a31d46d8c68cce813e07aaf6fe1f
WernervRensburg <werner9630@gmail.com>
2025-05-07 09:16:53 +0200

    basic layout for projects done, contact WIP

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,11 +1,44 @@
 function Contact() {
   return (
     <>
-      <div className="border border-orange-500">
-        This is the Contact Section
+      <div className="w-full max-w-7xl justify-center grid grid-cols-1 gap-4 border border-orange-500">
+        <div className="w-full flex justify-center max-w-[calc(100vw-6rem)] mx-auto border border-black">
+          contact us call now
+        </div>
+        <div className="w-full border mx-auto max-w-[calc(100vw-6rem)] border-blue-500">
+          <div className="grid grid-cols-2 gap-2 lg:grid-cols-4">
+            <div className="bg-gray-200 p-4 border border-green-700">Box 1</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 2</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 3</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 4</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 5</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 6</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 7</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 8</div>
+          </div>
+        </div>
+        <div className="w-full flex justify-center mx-auto max-w-[calc(100vw-6rem)] border border-black">
+          Social media pages
+        </div>
+      </div>
+      <div className="w-full flex justify-center border border-red-700">
+        <div className="w-full max-w-[calc(100vw-6rem)] grid grid-cols-1 gap-4">
+          <div className="relative">
+          <img
+            src="https://placehold.co/3000x2000"
+            alt="temporary-image"
+            className="w-full h-auto"
+            loading="lazy"
+          />
+          </div>
+          <div>
+
+          </div>
+        </div>
+
       </div>
     </>
-  )
+  );
 }
 
 export default Contact;
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,11 +1,27 @@
 function Projects() {
   return (
     <>
-      <div className="border border-indigo-500">
-        This is the Projects Section
+      <div className="flex justify-center">
+        <div className="max-w-7xl">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 w-5xl mx-auto m-4 max-w-[calc(100vw-4rem)] sm:border sm:border-black">
+            <div className="bg-gray-200 border border-green-700">
+              <div className="h-140 md:h-160 border border-red-400">box 1 IMAGE</div>
+              <div className="h-70 border border-blue-300">box 1 DESCRIPTION</div>
+            </div>
+            <div className="bg-gray-200 border border-green-700">
+              <div className="h-70 border border-red-400 hidden sm:block">box 2 DESCRIPTION</div>
+              <div className="h-140 md:h-160 border border-blue-300">box 2 IMAGE</div>
+              <div className="h-70 border border-red-400 block sm:hidden">box 2 small DESCRIPTION</div>
+            </div>
+            <div className="bg-gray-200 border border-green-700">
+              <div className="h-140 md:h-160 border border-red-400">box 3 IMAGE</div>
+              <div className="h-70 border border-blue-300">box 3 DESCRIPTION</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Projects;
