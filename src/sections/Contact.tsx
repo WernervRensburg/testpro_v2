4c8ffb462c341ddf290824e68f0cc944933bd0f6
WernervRensburg <werner9630@gmail.com>
2025-05-07 17:16:14 +0200

    Started with contact us basic layout

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,5 @@
+import { Input, Button } from "@headlessui/react";
+
 function Contact() {
   return (
     <>
@@ -21,18 +23,30 @@ function Contact() {
           Social media pages
         </div>
       </div>
-      <div className="w-full flex justify-center border border-red-700">
-        <div className="w-full max-w-[calc(100vw-6rem)] grid grid-cols-1 gap-4">
-          <div className="relative">
+      <div className="w-full relative justify-center border border-red-700">
           <img
             src="https://placehold.co/3000x2000"
             alt="temporary-image"
             className="w-full h-auto"
             loading="lazy"
           />
+        <div className="w-full absolute inset-0 mx-auto max-w-[calc(100vw-6rem)] grid grid-cols-1 gap-4 items-start mt-16">
+          <div className="border border-blue-700 h-fit">
+            <span className="font-bold">Don't Hesitate. Reach out to your local expert.</span>
           </div>
-          <div>
-
+          <div className="border border-green-700">
+            <div>
+            <Input></Input>
+            <Input></Input>
+            <Button className="rounded border border-blue-800 px-4 py-2">SUBMIT</Button>
+            </div>
+          </div>
+          <div className="border border-yellow-700">
+          <div className="grid grid-cols-1 gap-2 lg:grid-cols-3">
+            <div className="bg-gray-200 p-4 border border-green-700">Box 1</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 2</div>
+            <div className="bg-gray-200 p-4 border border-green-700">Box 3</div>
+          </div>
           </div>
         </div>
 
